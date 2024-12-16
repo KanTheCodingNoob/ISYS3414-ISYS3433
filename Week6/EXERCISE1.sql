60c10b0770c1222681b31ed3b8685967fbbe3c40
Vo Huy Chu <s4075654@rmit.edu.vn>
2024-12-16 14:21:01 +0700

    Finished exercise 1 of week 6

--- Week6/EXERCISE1.sql
+++ Week6/EXERCISE1.sql
@@ -7,18 +7,36 @@ FROM employee JOIN employee AS employees
 ON employee.department = employees.department
 AND employee.name < employees.name;
 
-WITH commission_and_history AS (
-SELECT commission.employee AS commission_employee, history.employee AS history_employee, commission.date, commission.value FROM commission LEFT JOIN history
-ON commission.employee = history.employee
-WHERE history.start_date < "2012-01-01"
-OR (history.start_date IS NULL AND commission.date <= "2012-01-01")
+WITH history_and_position AS (
+	SELECT history.*, position.code, position.income
+	FROM history LEFT OUTER JOIN position
+    	ON history.position = position.code  
+),
+history_before_20120101_and_position AS (
+	SELECT *
+    	FROM history_and_position
+    	WHERE start_date < '2012-01-01'  
+),
+history_after_20120101_and_position AS (
+    	SELECT *
+    	FROM history_and_position
+    	WHERE start_date >= '2012-01-01'
 )
+SELECT history_after_20120101_and_position.income - history_before_20120101_and_position.income AS income_increasement
+FROM history_after_20120101_and_position JOIN history_before_20120101_and_position
+ON history_after_20120101_and_position.employee = history_before_20120101_and_position.employee
+WHERE history_after_20120101_and_position.income - history_before_20120101_and_position.income = 50;
 
+SELECT name, position
+FROM employee LEFT JOIN position
+ON employee.position = position.code
+WHERE income IN (
+    	SELECT MAX(income) FROM position  
+);
 
-
-
-SELECT name, position,  FROM employee LEFT JOIN 
-ON employee.number = commission.employee
+SELECT number, name, income
+FROM employee LEFT JOIN position
+ON employee.position = position.code
 WHERE name REGEXP "^L.{3,}$";
 
 SELECT number, name, position FROM employee
@@ -30,9 +48,24 @@ WHERE department = "DP2";
 SELECT department.name, employee.name
 FROM department JOIN employee
 ON department.code = employee.department
-AND (department.director = employee.number
-OR employee.number IN (
-SELECT supervisor FROM employee
-WHERE supervisor IS NOT NULL
-));
+WHERE employee.supervisor IS NULL
+UNION
+SELECT department.name, employee.name
+FROM department JOIN employee
+ON department.director = employee.number;
 
+SELECT department.name, employee.name
+FROM department JOIN employee
+ON department.code = employee.department
+WHERE employee.supervisor IS NULL
+UNION
+SELECT department.name, employee.name
+FROM department JOIN employee
+ON department.director = employee.number;
+
+SELECT DISTINCT position.code, department.name
+FROM position
+RIGHT JOIN employee
+ON position.code = employee.position
+LEFT JOIN department
+ON employee.department = department.code;
